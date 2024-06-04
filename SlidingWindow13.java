f3094e59873eab9ce4ec3ed306f4c9cf09fb805d
Saket Jain <jsaket058@gmail.com>
2024-06-04 18:18:43 +0530

    New

--- SlidingWindow13.java
+++ SlidingWindow13.java
@@ -1,9 +1,22 @@
 import java.util.*;
 class SlidingWindow13{
-	static int longestSubstring(String s){
+	static int longestSubstring(String s,int k){
 		HashMap<Character,Integer> mpp=new HashMap<>();
+		int left=0,right=0,maxlen=0;
+		int n=s.length();
+		while(right < n){
+            mpp.put(s.charAt(right),mpp.getOrDefault(s.charAt(right),0)+1);
+			if(mpp.size()>k){
+				mpp.put(s.charAt(left),mpp.get(s.charAt(left))-1);
+				left++;
+			}
+			if(mpp.size()<=k)maxlen=Math.max(maxlen,right-left+1);
+			right++;
+		}
+		return maxlen;
 	}
 	public static void main(String[] args){
-		
+		String s="aaabbccd";
+		System.out.println(longestSubstring(s, 2));
 	}
 }
